17c361795adb64b6cdf13754a9bbced91d11a364
quotto <5211139+quotto@users.noreply.github.com>
2023-08-13 12:04:37 +0000

    apiの追加

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 function App() {
+  // APIリクエストを送信する関数
+  const sendApiRequest = async () => {
+    // APIリクエストを送信
+    const res = await fetch('/api/demo');
+    // JSON形式でレスポンスを受け取る
+    const data = await res.json();
+    // レスポンスの表示
+    console.log(data);
+  };
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+  <div>
+    <p>Hello {value}</p>
+    <button onClick={()=>sendApiRequest()}>Click me</button>
+  </div>);
 }
 
 export default App;
